941b84e97c4b3abe59aad77152d7423dc8a4afa8
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-05-26 10:46:42 +0100

    First commit

---
+++ src/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * Created by daveg on 15/05/2017.
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Dave!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int newTotal = 1000 - myTotal;
+
+        System.out.println(newTotal);
+
+    }
+}
